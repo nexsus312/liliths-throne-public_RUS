c53dff78c5175d12a3a314182000aecd75904547
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-04-18 20:49:52 +0300

    Перевод DifficultyLevel.java v1
    Перевод уровней сложности, пока не проверен!

--- src/com/lilithsthrone/game/settings/DifficultyLevel.java
+++ src/com/lilithsthrone/game/settings/DifficultyLevel.java
@@ -10,15 +10,15 @@ import com.lilithsthrone.utils.colours.PresetColour;
  */
 public enum DifficultyLevel {
 	
-	NORMAL("Human", "The way the game is meant to be played. No level scaling and no damage modifications.", PresetColour.RACE_HUMAN, false, 1, 1),
+	NORMAL("Человек", "Так как подразумевает игра. Уровень врагов и урон не зависит от уровня игрока.", PresetColour.RACE_HUMAN, false, 1, 1),
 
-	LEVEL_SCALING("Morph", "Enemies level up alongside your character, but do normal damage.", PresetColour.RACE_CAT_MORPH, true, 1, 1),
+	LEVEL_SCALING("Мутант", "Уровень врагов зависит от вашего, но урон не меняется.", PresetColour.RACE_CAT_MORPH, true, 1, 1),
 	
-	HARD("Demon", "Enemies level up alongside your character and do 200% damage.", PresetColour.RACE_DEMON, true, 2, 1),
+	HARD("Демон", "Уровень врагов зависит от вашего и они наносят 200% урона.", PresetColour.RACE_DEMON, true, 2, 1),
 	
-	NIGHTMARE("Lilin", "Enemies level up alongside your character, do 200% damage, and take only 50% damage from all sources.", PresetColour.BASE_PURPLE, true, 2, 0.5f),
+	NIGHTMARE("Лилин", "Уровень врагов зависит от вашего. Они наносят 200% урона и получают только 50% урона из всех источников.", PresetColour.BASE_PURPLE, true, 2, 0.5f),
 	
-	HELL("Lilith", "Enemies are always 2x your character's level, do 400% damage, and take only 25% damage from all sources. Be prepared to lose. A lot.", PresetColour.BASE_CRIMSON, true, 4, 0.25f);
+	HELL("Лилит", "Уровень врагов всегда в два раза выше вашего, они наносят 400% урона и получают только 25% урона из всех источников. Готовьтесь к поражениями. Их будет очень много.", PresetColour.BASE_CRIMSON, true, 4, 0.25f);
 
 	private String name;
 	private String description;
