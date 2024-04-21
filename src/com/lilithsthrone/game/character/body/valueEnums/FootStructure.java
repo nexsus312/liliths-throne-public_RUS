ededb7fbed1dd703a86ccc031108251c8117dcad
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-04-21 17:17:33 +0300

    Перевод FootStructure.java

--- src/com/lilithsthrone/game/character/body/valueEnums/FootStructure.java
+++ src/com/lilithsthrone/game/character/body/valueEnums/FootStructure.java
@@ -8,22 +8,22 @@ package com.lilithsthrone.game.character.body.valueEnums;
 public enum FootStructure {
 
 	/**no feet*/
-	NONE("none", "[npc.She] [npc.do] not have feet."),
+	NONE("нет", "[npc.She] [npc.do] не имеет ног."),
 	
 	/**walk with feet flat on the ground*/
-	PLANTIGRADE("plantigrade", "[npc.She] [npc.verb(walk)] with [npc.her] feet flat on the ground."),
+	PLANTIGRADE("плантиградное", "[npc.She] [npc.verb(walk)] со [npc.her] стопами плоско на земле."),
 
 	/**walk on toes with the heel permanently raised*/
-	DIGITIGRADE("digitigrade", "[npc.She] [npc.verb(walk)] on [npc.her] [npc.toes], with [npc.her] heel being permanently raised."),
+	DIGITIGRADE("дигитиградное", "[npc.She] [npc.verb(walk)] на [npc.her] [npc.toes], с помощью [npc.her] пяток которые постоянно подняты."),
 
 	/**walk on hoof with the rest of the foot permanently raised*/
-	UNGULIGRADE("unguligrade", "[npc.She] [npc.verb(walk)] on [npc.her] [npc.toes], with the rest of [npc.her] foot being permanently raised."),
+	UNGULIGRADE("унгулиградное", "[npc.She] [npc.verb(walk)] на [npc.her] [npc.toes], с остальной частью [npc.her] стопы, которая постоянно поднята."),
 
 	/**have segmented legs like a spider, so foot is the 'tarsus' segment.*/
-	ARACHNOID("arachnoid", "[npc.She] [npc.verb(walk)] on the ends of [npc.her] segmented arachnoid legs."),
+	ARACHNOID("арахнид", "[npc.She] [npc.verb(walk)] на концах [npc.her] сегментированных паучьх ног."),
 	
 	/**use tentacle-legs to walk around on*/
-	TENTACLED("tentacled", "[npc.She] [npc.verb(use)] the lower parts of [npc.her] tentacles to walk around on.");
+	TENTACLED("щупальцевое", "[npc.She] [npc.verb(use)] нижние части [npc.her] щупалец для передвижения.");
 	
 	private String name;
 	private String description;
