ea61230b462f008ab297f2fa19069ca686ead713
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-04-21 13:56:51 +0300

    Перевод AgeCategory.java

--- src/com/lilithsthrone/game/character/body/valueEnums/AgeCategory.java
+++ src/com/lilithsthrone/game/character/body/valueEnums/AgeCategory.java
@@ -15,39 +15,39 @@ import com.lilithsthrone.utils.colours.PresetColour;
 public enum AgeCategory {
 	
 	// Always at least 18, as returned by valueOf()
-	TEENS_LATE("late teens", 18, 20, PresetColour.AGE_TEENS, ContentPreferenceValue.FOUR_HIGH),
+	TEENS_LATE("больше восемнадцати", 18, 20, PresetColour.AGE_TEENS, ContentPreferenceValue.FOUR_HIGH),
 	
-	TWENTIES_EARLY("early twenties", 20, 23, PresetColour.AGE_TWENTIES, ContentPreferenceValue.FIVE_ABUNDANT),
+	TWENTIES_EARLY("около двадцати", 20, 23, PresetColour.AGE_TWENTIES, ContentPreferenceValue.FIVE_ABUNDANT),
 	
-	TWENTIES_MIDDLE("mid-twenties", 23, 27, PresetColour.AGE_TWENTIES, ContentPreferenceValue.FIVE_ABUNDANT),
+	TWENTIES_MIDDLE("около двадцатипяти", 23, 27, PresetColour.AGE_TWENTIES, ContentPreferenceValue.FIVE_ABUNDANT),
 	
-	TWENTIES_LATE("late twenties", 27, 30, PresetColour.AGE_TWENTIES, ContentPreferenceValue.FOUR_HIGH),
+	TWENTIES_LATE("почти тридцать", 27, 30, PresetColour.AGE_TWENTIES, ContentPreferenceValue.FOUR_HIGH),
 	
-	THIRTIES_EARLY("early thirties", 30, 33, PresetColour.AGE_THIRTIES, ContentPreferenceValue.THREE_AVERAGE),
+	THIRTIES_EARLY("в районе тридцати", 30, 33, PresetColour.AGE_THIRTIES, ContentPreferenceValue.THREE_AVERAGE),
 	
-	THIRTIES_MIDDLE("mid-thirties", 33, 37, PresetColour.AGE_THIRTIES, ContentPreferenceValue.THREE_AVERAGE),
+	THIRTIES_MIDDLE("больше тридцати", 33, 37, PresetColour.AGE_THIRTIES, ContentPreferenceValue.THREE_AVERAGE),
 	
-	THIRTIES_LATE("late thirties", 37, 40, PresetColour.AGE_THIRTIES, ContentPreferenceValue.TWO_LOW),
+	THIRTIES_LATE("почти сорок", 37, 40, PresetColour.AGE_THIRTIES, ContentPreferenceValue.TWO_LOW),
 	
-	FORTIES_EARLY("early forties", 40, 43, PresetColour.AGE_FORTIES, ContentPreferenceValue.TWO_LOW),
+	FORTIES_EARLY("в районе сорока", 40, 43, PresetColour.AGE_FORTIES, ContentPreferenceValue.TWO_LOW),
 	
-	FORTIES_MIDDLE("mid-forties", 43, 47, PresetColour.AGE_FORTIES, ContentPreferenceValue.ONE_MINIMAL),
+	FORTIES_MIDDLE("больше сорока", 43, 47, PresetColour.AGE_FORTIES, ContentPreferenceValue.ONE_MINIMAL),
 	
-	FORTIES_LATE("late forties", 47, 50, PresetColour.AGE_FORTIES, ContentPreferenceValue.ONE_MINIMAL),
+	FORTIES_LATE("почти пятьдесят", 47, 50, PresetColour.AGE_FORTIES, ContentPreferenceValue.ONE_MINIMAL),
 	
-	FIFTIES_EARLY("early fifties", 50, 53, PresetColour.AGE_FIFTIES, ContentPreferenceValue.ONE_MINIMAL),
+	FIFTIES_EARLY("в районе пятидесяти", 50, 53, PresetColour.AGE_FIFTIES, ContentPreferenceValue.ONE_MINIMAL),
 	
-	FIFTIES_MIDDLE("mid-fifties", 53, 57, PresetColour.AGE_FIFTIES, ContentPreferenceValue.ZERO_NONE),
+	FIFTIES_MIDDLE("больше пятидесяти", 53, 57, PresetColour.AGE_FIFTIES, ContentPreferenceValue.ZERO_NONE),
 	
-	FIFTIES_LATE("late fifties", 57, 60, PresetColour.AGE_FIFTIES, ContentPreferenceValue.ZERO_NONE),
+	FIFTIES_LATE("почти шестьдесят", 57, 60, PresetColour.AGE_FIFTIES, ContentPreferenceValue.ZERO_NONE),
 	
-	SIXTIES_EARLY("early sixties", 60, 63, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE),
+	SIXTIES_EARLY("в районе шестидесяти", 60, 63, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE),
 	
-	SIXTIES_MIDDLE("mid-sixties", 63, 67, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE),
+	SIXTIES_MIDDLE("больше шестидесяти", 63, 67, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE),
 	
-	SIXTIES_LATE("late sixties", 67, 70, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE),
+	SIXTIES_LATE("почти семьдесят", 67, 70, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE),
 	
-	SIXTIES_PLUS("past seventy", 70, 100, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE);
+	SIXTIES_PLUS("за семьдесят", 70, 100, PresetColour.AGE_SIXTIES, ContentPreferenceValue.ZERO_NONE);
 
 	private String name;
 	private int minimumAge;
