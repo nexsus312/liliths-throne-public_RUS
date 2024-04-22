52795e0e3abc103471146cb99e27a36162292a01
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-04-22 21:04:53 +0300

    Перевод PenisLength.java

--- src/com/lilithsthrone/game/character/body/valueEnums/PenisLength.java
+++ src/com/lilithsthrone/game/character/body/valueEnums/PenisLength.java
@@ -12,31 +12,31 @@ import com.lilithsthrone.utils.colours.PresetColour;
  */
 public enum PenisLength {
 	/**Utility*/
-	NEGATIVE_UTILITY_VALUE("N/A", -1, -1, PresetColour.GENERIC_SIZE_ONE),
+	NEGATIVE_UTILITY_VALUE("нет", -1, -1, PresetColour.GENERIC_SIZE_ONE),
 
 	/**Barely even anything there. (0-1 inches)*/
-	ZERO_MICROSCOPIC("tiny", 0, 5, PresetColour.GENERIC_SIZE_TWO),
+	ZERO_MICROSCOPIC("крохотный", 0, 5, PresetColour.GENERIC_SIZE_TWO),
 
 	/**Average size for a harpy. (2-3 inches)*/
-	ONE_TINY("small", 5, 10, PresetColour.GENERIC_SIZE_TWO),
+	ONE_TINY("маленький", 5, 10, PresetColour.GENERIC_SIZE_TWO),
 
 	/**Average size for all regular morphs. (4-7 inches)*/
-	TWO_AVERAGE("average-sized", 10, 20, PresetColour.GENERIC_SIZE_THREE),
+	TWO_AVERAGE("средний", 10, 20, PresetColour.GENERIC_SIZE_THREE),
 
 	/**Average size for a wolf morph. (8-11 inches)*/
-	THREE_LARGE("large", 20, 30, PresetColour.GENERIC_SIZE_FOUR),
+	THREE_LARGE("большой", 20, 30, PresetColour.GENERIC_SIZE_FOUR),
 
 	/**Average size for a horse morph. (12-15 inches)*/
-	FOUR_HUGE("huge", 30, 40, PresetColour.GENERIC_SIZE_FIVE),
+	FOUR_HUGE("очень большой", 30, 40, PresetColour.GENERIC_SIZE_FIVE),
 
 	/**Straying into the bounds of "world record for a human". Large for a horse morph. (16-19 inches)*/
-	FIVE_ENORMOUS("enormous", 40, 50, PresetColour.GENERIC_SIZE_SIX),
+	FIVE_ENORMOUS("огромный", 40, 50, PresetColour.GENERIC_SIZE_SIX),
 
 	/**This is just ridiculous... World record for a horse-morph. (20-23 inches)*/
-	SIX_GIGANTIC("gigantic", 50, 60, PresetColour.GENERIC_SIZE_SEVEN),
+	SIX_GIGANTIC("гигантский", 50, 60, PresetColour.GENERIC_SIZE_SEVEN),
 
 	/**And this is for "extreme proportion" content. (24-40 inches)*/
-	SEVEN_STALLION("stallion-sized", 60, 100, PresetColour.GENERIC_SIZE_EIGHT);
+	SEVEN_STALLION("как у коня", 60, 100, PresetColour.GENERIC_SIZE_EIGHT);
 
 	private int minimumValue, maximumValue;
 	private String descriptor;
