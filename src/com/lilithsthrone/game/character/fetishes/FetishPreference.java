80be5ca6ceabf98938174bd08f10826c07dfdc7a
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-04-23 16:19:17 +0300

    Перевод FetishPreference.java

--- src/com/lilithsthrone/game/character/fetishes/FetishPreference.java
+++ src/com/lilithsthrone/game/character/fetishes/FetishPreference.java
@@ -10,13 +10,13 @@ import com.lilithsthrone.utils.colours.PresetColour;
  */
 public enum FetishPreference {
 	
-	ZERO_DISABLED(PresetColour.TEXT_GREY, "disabled", 0, 0, 0, 0, 0, "This fetish will not be given to any NPC unless special conditions are met."),
-	ONE_HATE(PresetColour.GENERIC_BAD, "hate", 1, 10, 5, 0, 0, "NPCs will only dislike or hate this fetish unless special conditions are met."),
-	TWO_DISLIKE(PresetColour.GENERIC_MINOR_BAD, "dislike", 2, 5, 10, 3, 1, "NPCs will more likely dislike/hate this fetish but can still like/love it."),
-	THREE_NEUTRAL(PresetColour.TEXT, "neutral", 3, 3, 3, 3, 3, "No preference either way."),
-	FOUR_LIKE(PresetColour.GENERIC_MINOR_GOOD, "like", 4, 1, 3, 10, 5, "NPCs will more likely like/love this fetish but can still dislike/hate it."),
-	FIVE_LOVE(PresetColour.GENERIC_GOOD, "love", 5, 0, 0, 3, 5, "NPCs will only like or love this fetish."),
-	SIX_ALWAYS(PresetColour.GENERIC_EXCELLENT, "always", 6, 0, 0, 0, 1, "NPCs will always have this fetish.");
+	ZERO_DISABLED(PresetColour.TEXT_GREY, "отключено", 0, 0, 0, 0, 0, "Этот фетиш не будет ни у одного NPC, за исключением особых условий."),
+	ONE_HATE(PresetColour.GENERIC_BAD, "ненавижу", 1, 10, 5, 0, 0, "NPC будут только `не люблю/ненавижу` этот фетиш, за исключением особых условий."),
+	TWO_DISLIKE(PresetColour.GENERIC_MINOR_BAD, "не люблю", 2, 5, 10, 3, 1, "NPC чаще будут `не люблю/ненавижу` по отношению к этому фетишу, но все еще могут появится `нравится/люблю`."),
+	THREE_NEUTRAL(PresetColour.TEXT, "нейтрально", 3, 3, 3, 3, 3, "Нет предпочтений."),
+	FOUR_LIKE(PresetColour.GENERIC_MINOR_GOOD, "нравится", 4, 1, 3, 10, 5, "NPC чаще будут использовать `нравится/люблю` по отношению к этому фетишу, но все еще могут появится `не люблю/ненавижу`."),
+	FIVE_LOVE(PresetColour.GENERIC_GOOD, "люблю", 5, 0, 0, 3, 5, "NPC будет `нравится` или `люблю` данный фетиш."),
+	SIX_ALWAYS(PresetColour.GENERIC_EXCELLENT, "всегда", 6, 0, 0, 0, 1, "NPC всегда будут иметь этот фетиш.");
 	
 	private Colour colour;
 	private String name;
