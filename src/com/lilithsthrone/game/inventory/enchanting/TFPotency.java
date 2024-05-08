273f367796dfe9fb9d6c095b81a480b418b7e430
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-05-08 13:18:01 +0300

    Пeревод TFPotency.java

--- src/com/lilithsthrone/game/inventory/enchanting/TFPotency.java
+++ src/com/lilithsthrone/game/inventory/enchanting/TFPotency.java
@@ -14,12 +14,12 @@ import com.lilithsthrone.utils.colours.PresetColour;
  */
 public enum TFPotency {
 	
-	MAJOR_DRAIN("Major Drain", PresetColour.GENERIC_TERRIBLE, 8, -3),
-	DRAIN("Drain", PresetColour.GENERIC_BAD, 4, -2),
-	MINOR_DRAIN("Minor Drain", PresetColour.GENERIC_MINOR_BAD, 1, -1),
-	MINOR_BOOST("Minor Boost", PresetColour.GENERIC_MINOR_GOOD, 1, 1),
-	BOOST("Boost", PresetColour.GENERIC_GOOD, 4, 2),
-	MAJOR_BOOST("Major Boost", PresetColour.GENERIC_EXCELLENT, 8, 3);
+	MAJOR_DRAIN("Большое уменьшение", PresetColour.GENERIC_TERRIBLE, 8, -3),
+	DRAIN("Уменьшение", PresetColour.GENERIC_BAD, 4, -2),
+	MINOR_DRAIN("Небольшое уменьшение", PresetColour.GENERIC_MINOR_BAD, 1, -1),
+	MINOR_BOOST("Небольшое увеличение", PresetColour.GENERIC_MINOR_GOOD, 1, 1),
+	BOOST("Увеличение", PresetColour.GENERIC_GOOD, 4, 2),
+	MAJOR_BOOST("Большое увеличение", PresetColour.GENERIC_EXCELLENT, 8, 3);
 	
 	private static List<TFPotency> allPotencies = new ArrayList<>();
 
