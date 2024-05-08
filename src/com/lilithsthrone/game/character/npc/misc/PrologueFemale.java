c9495a4c5c127e88b109581bbfca06573111b9d0
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-05-08 13:34:14 +0300

    Перевод PrologueFemale.java

--- src/com/lilithsthrone/game/character/npc/misc/PrologueFemale.java
+++ src/com/lilithsthrone/game/character/npc/misc/PrologueFemale.java
@@ -62,8 +62,8 @@ public class PrologueFemale extends NPC {
 	}
 	
 	public PrologueFemale(boolean isImported) {
-		super(isImported, new NameTriplet("Alexander", "Alex", "Alexandria"), "Bonheur",
-				"One of the guests at the museum's opening exhibit. She's got a beautiful face and figure, and, even better, she seems to have taken an instant liking towards you...",
+		super(isImported, new NameTriplet("Александр", "Алекс", "Александрия"), "Счастье",
+				"Одна из гостей на открытии выставки в музее. У нее прекрасное лицо и фигура, и, что еще лучше, она, кажется, сразу же запала на вас...",
 				26, Month.APRIL, 12,
 				3,
 				Gender.F_V_B_FEMALE,
