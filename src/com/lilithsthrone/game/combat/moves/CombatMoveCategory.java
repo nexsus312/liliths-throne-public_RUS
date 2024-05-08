81f2c7a94456cdb98e6988bfd3e9d5a2368a33b9
nexsus312 <109859721+nexsus312@users.noreply.github.com>
2024-05-08 19:05:10 +0300

    Перевод не требуется CombatMoveCategory.java

--- src/com/lilithsthrone/game/combat/moves/CombatMoveCategory.java
+++ src/com/lilithsthrone/game/combat/moves/CombatMoveCategory.java
@@ -2,7 +2,7 @@ package com.lilithsthrone.game.combat.moves;
 
 /**
  * Category of CombatType for use in organising CombatMoves in menus.
- * 
+ * Перевод не требуется
  * @since 0.4
  * @version 0.4
  * @author Innoxia
